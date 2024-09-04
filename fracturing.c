5ed017481854b60125bd28184f66d398fcce9442
Jah'Nasia Whitehead <ja219627@ucf.edu>
2024-09-04 18:07:47 +0000

    lala

---
+++ fracturing.c
@@ -0,0 +1,12 @@
+//************
+// assignment 1
+// Author: Whitehead, Jah'Nasia
+// UCF ID: 5547687
+// Class: COP 3223
+// Instructor: Juan Parra
+// Purpose: This program prints a specific message to the
+// command line. The main purpose of this assignment is for 
+// the user to input distance, perimeter, width, height, and area. 
+// Input: definitions
+//
+// Output: (To the command line) 2 points.
