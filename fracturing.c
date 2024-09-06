cc3cbecd240c9d912835c78d76fe918846673760
Jah'Nasia Whitehead <ja219627@ucf.edu>
2024-09-06 20:23:37 +0000

    8th commit

--- fracturing.c
+++ fracturing.c
@@ -9,20 +9,19 @@
 #include <stdio.h>
 #include <math.h>
 
-double findDistance() {
-    
-    double x1, y1, x2, y2;
-    
-    // Ask for inputs
-    x1= printf("Enter the coordinates of the X in the first point: x1=");
-    scanf("%lf", &x1);
-    y1= printf("Enter the coordinates of Y in the first point: y1=");
-    scanf("%lf", &y1);
-    x2= printf("Enter the coordinates of the X in the second point: x1=");
-    scanf("%lf", &x2);
-    y2= printf("Enter the coordinates of Y in the second point: y2=");
-    scanf("%lf", &y2);
+// Ask for inputs
+printf("Enter the coordinates of the X in the first point: x1=");
+scanf("%lf", &x1);
+printf("Enter the coordinates of Y in the first point: y1=");
+scanf("%lf", &y1);
+printf("Enter the coordinates of the X in the second point: x1=");
+scanf("%lf", &x2);
+printf("Enter the coordinates of Y in the second point: y2=");
+scanf("%lf", &y2);
 
+
+double findDistance() {
+    double x1, y1, x2, y2;
     //double x1,y1, x2, y2 distance;?? is this needed?
     // if I return sqrt will it work?? or is it seperate. 
     return sqrt(pow(x2-x1,2)+pow(y2-y1,2));
@@ -30,17 +29,23 @@ double findDistance() {
 // Distance 
  double calculateDistance() {
     double distance= findDistance();
-    printf("The distance between the 2 points is: %.2lf",distance,"\n\n");
+    printf("The distance between the 2 points: %.2lf",distance,"\n\n");
     return distance;
 
     }
+
+double findPerimeter() {
+    
+    double x1, y1, x2, y2;
+    return x1+y1+x2+y2;
+}
 // Perimeter
  double calculatePerimeter() {
-    double diameter = x1+x2+y1+y2;
-    double perimeter = diameter;
-    printf("The perimeter of the city is %.2lf", perimeter,"\n\n");
+    double perimeter = findPerimeter();
+    printf("The perimeter of the city is %.2d", perimeter,"\n\n");
+
     return 0;
-    }
+   }
 
 
 
