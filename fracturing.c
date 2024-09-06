24b17d67b56e6e5766dc644b0214cd6055d752bf
Jah'Nasia Whitehead <ja219627@ucf.edu>
2024-09-06 19:32:21 +0000

    fourth commit

--- fracturing.c
+++ fracturing.c
@@ -4,43 +4,56 @@
 // UCF ID: 5547687
 // Class: COP 3223
 // Instructor: Juan Parra
-// Purpose: This program prints a specific message to the
-// command line. The main purpose of this assignment is for 
-// the user to input distance, perimeter, width, height, and area. 
-// Input: definitions
-//
-// Output: (To the command line) 2 points.
+
 
 #include <stdio.h>
 #include <math.h>
 
-double calculateDistance(double x1, double y1, double x2, double y2){
+double findDistance() {
     
-    //int x1, y1, x2, y2;
+    double x1, y1, x2, y2;
     
     // Ask for inputs
-    printf("Enter the coordinates of the first point (x1,y1):");
-    scanf("%lf %lf", &x1, &y1);
-    printf("Enter the coordinates of the second point (x2,y2):");
-    scanf("%lf %lf", &x2, &y2);
-    sqrt(pow(x2-x1,2)+pow(y2-y1,2));
+    printf("Enter the coordinates of the X in the first point: x1=");
+    scanf("%lf", &x1);
+    printf("Enter the coordinates of Y in the first point: y1=");
+    scanf("%lf", &y1);
+    printf("Enter the coordinates of the X in the second point: x1=");
+    scanf("%lf", &x2);
+    printf("Enter the coordinates of Y in the second point: y2=");
+    scanf("%lf", &y2);
 
     //double x1,y1, x2, y2 distance;?? is this needed?
-
-    return 2.0;
+    // if I return sqrt will it work?? or is it seperate. 
+    return sqrt(pow(x2-x1,2)+pow(y2-y1,2));
 }
-int main(int argc, char **argv)
-{
-    double calculateDistance();
+// Distance 
+ double calculateDistance() {
+    double distance= findDistance();
+    printf("The distance between the 2 points is: %.2lf",distance);
+    return distance;
+
+    }
+// Perimeter
+ double calculatePerimeter() {
+    double distance = findDistance();
+    double perimeter = x1 + y1 + x2 + y2;
+    printf("The perimeter of the city is %.2lf", perimeter);
+    return 0;
+    }
+
+
+
+ 
+
     
-    //calculatePerimeter();
 
-    //calculateArea();
-
-    //calculateWidth();
-
-    //calculateHeight();
   
-    return 0; 
-  
+int main()
+{
+    calculateDistance();
+    calculatePerimeter();
+
+    return 0;
 }
+  
